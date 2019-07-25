1d2a706b57c9593269e24eb05369e384f5308467
Gregor <gregorlthomson@sky.com>
2019-07-25 14:59:51 +0100

    Test filterResults returns 5 results

--- src/test.js
+++ src/test.js
@@ -19,3 +19,20 @@ test("Check filterResults returns an array", function(t) {
   t.equal(actual, expected, "Function should return an array");
   t.end();
 });
+
+test("Check filterResults returns an array of 5", function(t) {
+  const movieObj = {
+    results: [
+      { title: "lion king" },
+      { title: "snow white" },
+      { title: "shrek" },
+      { title: "lion king 2" },
+      { title: "snow white 2" },
+      { title: "shrek the Third" }
+    ]
+  };
+  const actual = handler.filterResults(movieObj).length;
+  const expected = 5;
+  t.equal(actual, expected, "Function should return an array of 5");
+  t.end();
+});
