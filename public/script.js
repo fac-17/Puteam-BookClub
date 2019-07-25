bcebc9f5b464871a8d4ac73122b69d98c4a0c7b5
Victor Masson <victormasson21@gmail.com>
2019-07-25 16:33:14 +0100

    Adding dropdown choices w/ placeholders

--- public/script.js
+++ public/script.js
@@ -11,3 +11,18 @@ document.querySelector(".form").addEventListener("submit", function(e) {
   var searchInput = document.querySelector(".form--input").value;
   apiCall(searchInput);
 });
+
+
+// TEMPORARY FUNCTION TO POPULATE SUGESTIONS LIST
+const tempChoices = ['ajllo', 'ajck', 'ajie', 'ajfhryg', 'ajdiauwfherufh']
+const autoContainer = document.querySelector(".autocomplete");
+const autoContainerItems = document.querySelector(".autocomplete-items");
+
+for (let i=0; i<tempChoices.length; i++) {
+
+  var newChoice = document.createElement('div')
+  newChoice.textContent = tempChoices[i];
+  newChoice.className = 'autocomplete-items'
+  autoContainer.appendChild(newChoice);
+
+}
