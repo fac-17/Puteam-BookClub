cd0d6162188af0e987e1f4f5126e7b3bdbb2b633
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-07-25 14:27:48 +0100

    Adds keydown eventlistener to script.js

--- public/call.js
+++ public/call.js
@@ -1,16 +1,15 @@
-let input = document.querySelector(".form--input");
+// let input = document.querySelector(".form--input");
 let xhr = new XMLHttpRequest();
-let url = "/search=" + input;
 
-const frontCall() => {
-xhr.onreadystatechange = function() => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    let frontObj = JSON.parse(xhr.responseText);
-  } else {
-    console.error(xhr.responseText);
-  }
-  xhr.open("GET", url, true);
-  xhr.send();
-  }
-
+const frontCall = function(input) {
+  let url = "/search=" + input;
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let frontObj = JSON.parse(xhr.responseText);
+    } else {
+      console.error(xhr.responseText);
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+  };
 };
